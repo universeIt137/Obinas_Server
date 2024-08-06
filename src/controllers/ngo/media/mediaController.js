5c5d3aab1db2ab3c0e99be20ab0e470323c5ccde
ishanrana363 <ishanrana363@gmail.com>
2024-08-07 01:24:21 +0600

    media post api created

--- src/controllers/ngo/media/mediaController.js
+++ src/controllers/ngo/media/mediaController.js
@@ -1,1 +1,33 @@
+const mediaModel = require("../../../models/ngo/mediaModel");
+const {parseUserToken} = require("../../../helper/helper");
 
+class mediaClass {
+    postMedia = async (req,res)=>{
+        let userToken = parseUserToken(req);
+        try {
+            let reqBody = req.body;
+            if (userToken.role==="super_admin"){
+                let data = await mediaModel.create(reqBody);
+                return res.status(201).json({
+                    status : "success",
+                    msg : data
+                });
+            }else {
+                return res.status(403).json({
+                    status : "fail",
+                    msg : "Permission not allow"
+                });
+            }
+        }catch (e) {
+            return res.status(500).json({
+                status:"fail",
+                msg:"something went worng"
+            });
+        }
+    };
+
+}
+
+const mediaController = new mediaClass();
+
+module.exports = mediaController
--- src/routes/privateRoutes.js
+++ src/routes/privateRoutes.js
@@ -1,10 +1,22 @@
 const express = require("express");
+
 const newsController = require("../controllers/ngo/news/newsController");
+
 const authMiddleware = require("../middlewares/authMiddleware");
+
+const mediaController = require("../controllers/ngo/media/mediaController");
 
 const router = express.Router();
 
+// news api
+
 router.post("/post-news" , authMiddleware.isSuperAdmin, newsController.postNews );
+
+
+
+// media api
+
+router.post("/post-media" , authMiddleware.isSuperAdmin, mediaController.postMedia );
 
 
 module.exports = router;
