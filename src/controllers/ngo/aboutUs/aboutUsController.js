2666ba7e3f1d3bb074a94dee462296d26f6ccb50
ashikur <ashikur1602@gmail.com>
2024-08-07 16:53:40 +0600

    successfully deployed on cpannel

--- src/controllers/ngo/aboutUs/aboutUsController.js
+++ src/controllers/ngo/aboutUs/aboutUsController.js
@@ -1,2 +1,10 @@
 const aboutUsModel = require("../../../models/ngo/aboutUs");
 
+exports.aboutUs = async (req, res) => {
+    try {
+        res.send("Server run successfully");
+    } catch (e) {
+        
+    }
+}
+
--- src/routes/privateRoutes.js
+++ src/routes/privateRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const newsController = require("../controllers/ngo/news/newsController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const aboutUsController = require("../controllers/ngo/aboutUs/aboutUsController")
 
 const router = express.Router();
 
-router.post("/post-news" , authMiddleware.isSuperAdmin, newsController.postNews );
+router.post("/post-news", authMiddleware.isSuperAdmin, newsController.postNews);
+
+router.get("/about", aboutUsController.aboutUs);
 
 
 module.exports = router;
