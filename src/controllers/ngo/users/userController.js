40d4b4178e94d85716f6d540bf3e78db79ce318e
ishanrana363 <ishanrana363@gmail.com>
2024-08-07 18:12:53 +0600

    user get profile api completed

--- src/controllers/ngo/users/userController.js
+++ src/controllers/ngo/users/userController.js
@@ -1,13 +1,36 @@
 const userModel = require("../../../models/ngo/userModel");
+const {parseUserToken} = require("../../../helper/helper");
 
 class userClass {
     getProfile = async (req,res)=>{
+        const userToken = parseUserToken(req);
         try {
-            
+            const userEmail = userToken.email;
+            let filter = {
+                email : userEmail
+            };
+            let data = await userModel.findOne(filter);
+
+            if (!data){
+                return res.status(404).json({
+                    status : "fail",
+                    msg : "User data not found"
+                });
+            }else {
+                return res.status(200).json({
+                    status : "success",
+                    data : data
+                });
+            }
         }catch (e) {
-            
+            return res.status(500).json({
+                status : "fail",
+                msg : "Something went worng"
+            });
         }
     }
 }
 
 const userController = new userClass();
+
+module.exports = userController;
--- src/routes/publicRoute.js
+++ src/routes/publicRoute.js
@@ -2,10 +2,28 @@ const express = require("express");
 
 const router = express.Router();
 
+//authController
+
 const authController = require("../controllers/ngo/auth/authController");
 
+// user controller
+
+const userController = require("../controllers/ngo/users/userController");
+
+// auth middleware
+
+const authMiddleware = require("../middlewares/authMiddleware");
+
+
+//auth route
+
 router.post("/sign-up", authController.signUp);
-router.get("/sign-in", authController.signIn )
+router.get("/sign-in", authController.signIn );
+
+
+// user route
+
+router.get("/get-profile", authMiddleware.isValidUser, userController.getProfile );
 
 
 module.exports = router;
