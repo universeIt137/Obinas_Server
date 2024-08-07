bfece3923dd1e5a96804995c0ef75e7f8f3d7090
ishanrana363 <ishanrana363@gmail.com>
2024-08-07 23:19:49 +0600

    concern delete api completed

--- src/controllers/ngo/concern/concernController.js
+++ src/controllers/ngo/concern/concernController.js
@@ -57,6 +57,38 @@ class concernClass  {
                 msg : e.toString()
             });
         }
+    };
+
+    deleteConcern = async (req,res)=>{
+        let userToken = parseUserToken(req);
+        try {
+            let id = req.params.id;
+            let filter = {
+                _id: id
+            };
+            let data = await concernModel.findById(filter);
+            if (!data) return res.status(404).json({
+                status : "success",
+                msg : "Data not found"
+            });
+            if ((userToken.role==="super-admin")){
+                await concernModel.findByIdAndDelete(filter);
+                return res.status(200).json({
+                    status:"success",
+                    msg : "Delete successfully"
+                });
+            }else {
+                return res.status(403).json({
+                    status : "fail",
+                    msg : "Permission not granted"
+                });
+            }
+        }catch (e) {
+            return res.status(500).json({
+                status : "fail",
+                msg : e.toString()
+            });
+        }
     }
 
 }
