6b65a3457f0652f695609f2e6743dfc8501accf0
ishanrana363 <ishanrana363@gmail.com>
2024-08-08 06:22:15 +0600

    update news api completed

--- src/controllers/ngo/news/newsController.js
+++ src/controllers/ngo/news/newsController.js
@@ -39,6 +39,39 @@ class newsClass {
             });
         }
     };
+    updateNews = async (req,res)=>{
+        const userToken = parseUserToken(req);
+        try {
+            const id = req.params.id;
+            const matchStage = {
+                _id : id
+            };
+            const reqBody = req.body;
+            let data = await newsModel.findById(matchStage);
+            if (!data) return res.status(404).json({
+                status : "success",
+                msg : "Data not found"
+            });
+            if ( userToken.role==="super-admin" ){
+                await newsModel.findByIdAndUpdate(matchStage,reqBody);
+                return res.status(200).json({
+                    status : "success",
+                    msg : `Update successfully`
+                });
+            }else {
+                return res.status(403).json({
+                    status : "fail",
+                    msg : 'Permission not granted'
+                });
+            }
+        }catch (e) {
+            return res.status(500).json({
+                status : "fail",
+                msg : e.toString()
+            });
+        }
+    }
+
 }
 
 const newsController = new newsClass();
