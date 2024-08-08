f2c25dfb9093e8bed0901545fd9c1b6e6a94494c
ishanrana363 <ishanrana363@gmail.com>
2024-08-08 23:22:19 +0600

    get single notice api complete

--- src/controllers/ngo/notice/noticeController.js
+++ src/controllers/ngo/notice/noticeController.js
@@ -24,7 +24,50 @@ class noticeClass {
             });
         }
     };
-
+    getAllNotice = async (req,res)=>{
+        try {
+            let data = await noticeModel.find();
+            if (data.length===0){
+                return res.status(404).json({
+                    status : "fail",
+                    msg : "Notice not found"
+                });
+            }else {
+                return res.status(200).json({
+                    status : "success",
+                    data : data
+                });
+            }
+        }catch (e) {
+            return res.status(500).json({
+                status : "fail",
+                msg : e.toString()
+            });
+        }
+    };
+    getSingleNotice = async (req,res)=>{
+      try {
+          let id = req.params.id;
+          let filter = { _id : id };
+          let data = await noticeModel.findById(filter);
+          if(!data){
+              return res.status(404).json({
+                  status : "fail",
+                  msg : "Notice not found"
+              });
+          }else {
+              return res.status(200).json({
+                  status : "success",
+                  data : data
+              });
+          }
+      }catch (e) {
+          return res.status(500).json({
+              status:"fail",
+              msg : e.toString()
+          });
+      }
+    };
 }
 
 const noticeController = new noticeClass();
