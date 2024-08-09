e0cf09571e95a4bfe6b484d9462caea35be25b51
ishanrana363 <ishanrana363@gmail.com>
2024-08-09 09:31:57 +0600

    get all applay service api complete

--- src/controllers/ngo/applayService/applayServiceController.js
+++ src/controllers/ngo/applayService/applayServiceController.js
@@ -25,6 +25,35 @@ class applayServiceClass {
             });
         }
     };
+    getAllApplayService = async (req,res)=>{
+        let userToken = parseUserToken(req);
+        try {
+            let data = await applayServiceModel.find();
+            if (data.length===0){
+                return res.status(404).json({
+                    status:"fail",
+                    msg : "Applay service not found"
+                });
+            }
+            if (userToken.role==="admin"){
+                let data = await applayServiceModel.find();
+                return res.status(200).json({
+                    status : "success",
+                    data : data
+                })
+            }else {
+                return res.status(403).json({
+                    status : "fail",
+                    msg : "Permission not allow"
+                });
+            }
+        }catch (e) {
+            return res.status(500).json({
+                status : "fail",
+                msg : e.toString()
+            });
+        }
+    };
 }
 
 const applayServiceController = new applayServiceClass();
