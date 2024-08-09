531ce66c94f95f819b33cea808601e0256616403
ishanrana363 <ishanrana363@gmail.com>
2024-08-09 11:34:27 +0600

    concern controllers all api modify

--- src/controllers/ngo/media/mediaController.js
+++ src/controllers/ngo/media/mediaController.js
@@ -6,7 +6,7 @@ class mediaClass {
         let userToken = parseUserToken(req);
         try {
             let reqBody = req.body;
-            if (userToken.role==="super-admin"){
+            if (userToken.role==="admin"){
                 let data = await mediaModel.create(reqBody);
                 return res.status(201).json({
                     status : "success",
@@ -25,42 +25,6 @@ class mediaClass {
             });
         }
     };
-    updateMedia = async (req,res)=>{
-        const userToken = parseUserToken(req);
-        try {
-            let id = req.params.id;
-            let matchStage = {
-                _id : id
-            };
-            let reqBody = req.body;
-
-            let data = await mediaModel.findById( matchStage );
-
-            if (!data) return res.status(404).json({
-                status : "fail",
-                msg : "Data not found"
-            });
-
-            if (userToken.role==="super-admin"){
-                await mediaModel.findByIdAndUpdate(matchStage,reqBody);
-                return res.status(200).json({
-                    status : "success",
-                    msg : "Update successfully"
-                });
-            }else {
-                return res.status(403).json({
-                    status : "fail",
-                    msg : "Permission not granted"
-                });
-            }
-
-        }catch (e) {
-            return res.status(500).json({
-                status : "fail",
-                msg : e.toString()
-            });
-        }
-    };
     deleteMedia = async (req,res)=>{
         const userToken = parseUserToken(req);
         try {
@@ -73,7 +37,7 @@ class mediaClass {
                 status : "fail",
                 msg : "Data not found"
             });
-            if (userToken.role==="super-admin"){
+            if (userToken.role==="admin"){
                 await mediaModel.findByIdAndDelete(matchStage);
                 return res.status(200).json({
                     status : "success",
@@ -115,37 +79,6 @@ class mediaClass {
             });
         }
     };
-
-    getAllMediaAdmin = async (req,res)=>{
-        const userToken = parseUserToken(req);
-        try {
-            let data = await mediaModel.find();
-            if (data.length===0){
-                return res.status(404).json({
-                    status : "fail",
-                    msg : "Data not found"
-                });
-            }else if ( userToken.role==="super-admin" ){
-                let data = await mediaModel.find();
-                return res.status(200).json({
-                    status : "success",
-                    data : data
-                });
-            }else {
-                return res.status(500).json({
-                    status : 'fail',
-                    msg : "Permission not granted"
-                });
-            }
-        }catch (e) {
-            return res.status(500).json({
-                status : "fail",
-                msg : e.toString()
-            });
-        }
-    };
-
-
 
 }
 
